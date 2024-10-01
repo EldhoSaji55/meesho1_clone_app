d383aadaca670e19db0a12662dd8f299e9e0b59d
EldhoSaji55 <eldhosaji4755@gmail.com>
2024-10-02 02:22:23 +0530

    AccountPage

--- lib/view/Account_Screen/accountScreen.dart
+++ lib/view/Account_Screen/accountScreen.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:meesho_clone_app/utils/constants/color_constants.dart';
 import 'package:meesho_clone_app/view/Global_widgets/AppbarWidget.dart';
 
 class Accountscreen extends StatelessWidget {
-  const Accountscreen({super.key});
+  Accountscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Column(
         children: [
@@ -16,10 +18,116 @@ class Accountscreen extends StatelessWidget {
             needSearch: true,
             needHeart: false,
           ),
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 10,
+                  ),
+                  _SignUpSection(),
+                  _AccountScreenListSection(),
+                  Container(
+                    height: 200,
+                    width: screenWidth,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image:
+                                AssetImage("assets/images/madeinIndia.png"))),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _AccountScreenListSection() {
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
           SizedBox(
-            height: 10,
+            height: 9,
           ),
-          _SignUpSection()
+          Text(
+            "My Payments",
+            style: TextStyle(
+                color: ColorConstants.Black.withOpacity(0.7),
+                fontWeight: FontWeight.w600),
+          ),
+          AccountListWidget(
+            prefixIcon: FontAwesomeIcons.ccAmazonPay,
+            title: "Bank & UPI Details",
+            iconColor: ColorConstants.Iconblue,
+          ),
+          AccountListWidget(
+            prefixIcon: FontAwesomeIcons.creditCard,
+            title: "Payment & Refund",
+            iconColor: ColorConstants.Iconblue,
+          ),
+          SizedBox(
+            height: 18,
+          ),
+          Text(
+            "My Activity",
+            style: TextStyle(
+                color: ColorConstants.Black.withOpacity(0.7),
+                fontWeight: FontWeight.w600),
+          ),
+          AccountListWidget(
+            prefixIcon: FontAwesomeIcons.solidHeart,
+            title: "Wishlisted Products",
+            iconColor: ColorConstants.mainred,
+          ),
+          AccountListWidget(
+            prefixIcon: FontAwesomeIcons.shareNodes,
+            title: "Shared Products",
+            iconColor: ColorConstants.primaryColor.withOpacity(.7),
+          ),
+          AccountListWidget(
+            prefixIcon: FontAwesomeIcons.shop,
+            title: "Followed Shops",
+            iconColor: ColorConstants.mainorange,
+          ),
+          SizedBox(
+            height: 18,
+          ),
+          Text(
+            "Others",
+            style: TextStyle(
+                color: ColorConstants.Black.withOpacity(0.7),
+                fontWeight: FontWeight.w600),
+          ),
+          AccountListWidget(
+            prefixIcon: FontAwesomeIcons.coins,
+            title: "Meesho Credits",
+            iconColor: ColorConstants.mainYellow,
+          ),
+          AccountListWidget(
+            prefixIcon: FontAwesomeIcons.briefcase,
+            title: "Become a Supplier",
+            iconColor: ColorConstants.mainBrown.withOpacity(.8),
+          ),
+          AccountListWidget(
+            prefixIcon: FontAwesomeIcons.gear,
+            title: "Settings",
+            iconColor: ColorConstants.mainGrey,
+          ),
+          AccountListWidget(
+            prefixIcon: FontAwesomeIcons.solidStar,
+            title: "Rate Meesho",
+            iconColor: ColorConstants.secondaryColor,
+          ),
+          AccountListWidget(
+            prefixIcon: FontAwesomeIcons.gavel,
+            title: "Legal and Policies",
+            iconColor: ColorConstants.mainBrown,
+          ),
         ],
       ),
     );
@@ -74,25 +182,123 @@ class Accountscreen extends StatelessWidget {
                             fontSize: 11,
                             color: ColorConstants.Black.withOpacity(0.8)),
                       ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Row(
-                        children: [
-                          Flexible(
-                              child: Container(
-                            decoration: BoxDecoration(),
-                          ))
-                        ],
-                      )
                     ],
                   ),
                 ),
               )
             ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            children: [
+              Flexible(
+                  flex: 1,
+                  child: Container(
+                    height: 80,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                          color: ColorConstants.Black.withOpacity(0.4)),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.phoneVolume,
+                          color: ColorConstants.Iconblue,
+                        ),
+                        Text(
+                          "Help Center",
+                          style: TextStyle(
+                              color: ColorConstants.Black.withOpacity(0.4),
+                              fontSize: 11,
+                              fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  )),
+              SizedBox(
+                width: 20,
+              ),
+              Flexible(
+                  flex: 1,
+                  child: Container(
+                    height: 80,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                          color: ColorConstants.Black.withOpacity(0.4)),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.language,
+                          color: ColorConstants.primaryColor.withOpacity(0.7),
+                        ),
+                        Text(
+                          "Change Language",
+                          style: TextStyle(
+                              color: ColorConstants.Black.withOpacity(0.4),
+                              fontSize: 11,
+                              fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  )),
+            ],
           )
         ],
       ),
+    );
+  }
+}
+
+class AccountListWidget extends StatelessWidget {
+  AccountListWidget({
+    required this.prefixIcon,
+    required this.title,
+    this.iconColor = ColorConstants.primaryColor,
+    super.key,
+  });
+  String title;
+  IconData prefixIcon;
+  Color iconColor;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          children: [
+            FaIcon(
+              prefixIcon,
+              color: iconColor,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Text(
+              title,
+              style: TextStyle(color: ColorConstants.Black.withOpacity(0.7)),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          height: 1,
+          width: double.infinity,
+          color: ColorConstants.colGrey,
+        ),
+      ],
     );
   }
 }
